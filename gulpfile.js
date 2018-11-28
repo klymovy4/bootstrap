4539c07d65871ef7e1ad19dc7d20a4275db49c8d
klymovy4 <klymovy4roman@gmail.com>
2018-11-28 08:31:07 +0200

    1

---
+++ gulpfile.js
@@ -0,0 +1,56 @@
+var 	gulp           = require('gulp'),
+		gutil          = require('gulp-util' ),
+		sass           = require('gulp-sass'),
+		browserSync    = require('browser-sync'),
+		concat         = require('gulp-concat'),
+		uglify         = require('gulp-uglify'),
+		cleanCSS       = require('gulp-clean-css'),
+		rename         = require('gulp-rename'),
+		autoprefixer   = require('gulp-autoprefixer'),
+		notify         = require("gulp-notify");
+		
+		
+
+// Сервер и автообновление страницы Browsersync
+gulp.task('browser-sync', function() {
+	browserSync({
+		server: {
+			baseDir: 'app'
+		},
+		notify: false,
+		// tunnel: true,
+		// tunnel: "projectmane", //Demonstration page: http://projectmane.localtunnel.me
+	});
+});
+
+// Минификация пользовательских скриптов проекта и JS библиотек в один файл
+gulp.task('js', function() {
+	return gulp.src([
+		'app/js/jquery-3.3.1.min.js',
+		'app/js/popper.min.js',
+		'app/js/bootstrap.min.js',
+		'app/js/mdb.min.js', // Всегда в конце
+		])
+	.pipe(concat('scripts.min.js'))
+	.pipe(uglify()) // Минимизировать весь js (на выбор)
+	.pipe(gulp.dest('app/js'))
+	.pipe(browserSync.reload({stream: true}));
+});
+
+gulp.task('sass', function() {
+	return gulp.src('app/scss/*.scss')
+	.pipe(sass({outputStyle: 'expand'}).on("error", notify.onError()))
+	.pipe(rename({suffix: '.min', prefix : ''}))
+	.pipe(autoprefixer(['last 2 versions']))
+	.pipe(cleanCSS()) // Опционально, закомментировать при отладке
+	.pipe(gulp.dest('app/css'))
+	.pipe(browserSync.reload({stream: true}));
+});
+
+gulp.task('watch', ['sass', 'js', 'browser-sync'], function() {
+	gulp.watch('app/scss/*.scss', ['sass']);
+	gulp.watch(['app/js/*.js'], ['js']);
+	gulp.watch('app/index.html', browserSync.reload);
+});
+
+gulp.task('default', ['watch']);
